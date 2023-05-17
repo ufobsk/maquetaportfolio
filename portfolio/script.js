549e787614210bab6e308284b43d36f8c11fb498
fnkbsk <igoico10@gmail.com>
2023-05-17 19:29:18 -0300

    codeJavaScript

---
+++ portfolio/script.js
@@ -0,0 +1,10 @@
+// Funcion que muestra el menu responsive
+
+function responsiveMenu () {
+    let x = documen.getElementById("nav");
+    if (x.className===""){
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
