7787def4fc7845e45f5e8c11a61be42f92b3c1b8
fnkbsk <igoico10@gmail.com>
2023-05-19 19:00:27 -0300

    newfunction

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,3 +1,21 @@
+// Funcion que aplica el estilo a la opcion seleccionada en el menu y quita la previamente sellecionada
+
+function seleccionar(link) {
+    let opciones = document.querySelector('#links a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+
+    // hacemos desaparecer el menu una vez que se ha seleccionado una opcion en el modo responsive
+    let x = document.getElementById("nav");
+    x.className = "";
+
+}
+
+
+
 // Funcion que muestra el menu responsive
 
 function responsiveMenu() {
